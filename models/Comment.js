43e966b2f8830d2cd5278e2074049dfd9ac94ac0
genevievem32-commits <genevieve.m.32@comets.kcsa.org>
2026-09-11 07:59:34 -0400

    Add Comment model

---
+++ models/Comment.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  authorName: String,
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post',
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
